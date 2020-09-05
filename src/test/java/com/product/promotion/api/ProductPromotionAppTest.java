36b485b046471fa6060b66e1d6adb639a230a4f3
raje231087 <rajesh.adyanadka@hotmail.com>
2020-09-05 20:37:32 +0530

    Test case update

---
+++ src/test/java/com/product/promotion/api/ProductPromotionAppTest.java
@@ -0,0 +1,17 @@
+package com.product.promotion.api;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProductPromotionAppTest {
+
+	@Test
+	public void mainTest() {
+		ProductPromotionApp.main(new String[] {});
+	}
+
+}
